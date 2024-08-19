cf3ec84d4f540e698bcd2d7fb0a2c3a73e525d92
DoubleLatte <85533915+DoubleLatte@users.noreply.github.com>
2024-08-19 18:55:55 +0900

    first
    upload

---
+++ src/main/java/me/ddlatte/auth/AUTH.java
@@ -0,0 +1,17 @@
+package me.ddlatte.auth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AUTH extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger("start")
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
